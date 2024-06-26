c2b13f946c726fec96cc8070f4e358bc63bbab69
Khashika14 <143623086+Khashika14@users.noreply.github.com>
2024-06-26 10:50:54 +0530

    Create safety_script.py

---
+++ safety_script.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+
+def run_safety():
+    requirements_file = 'test_web/requirements.txt'
+    if os.path.exists(requirements_file):
+        result = subprocess.run(['safety', 'check', '-r', requirements_file], capture_output=True, text=True)
+        print(result.stdout)
+        if result.returncode != 0:
+            raise Exception("Dependency issues found")
+    else:
+        print(f"{requirements_file} not found, skipping dependency scanning.")
+
+if __name__ == "__main__":
+    run_safety()
